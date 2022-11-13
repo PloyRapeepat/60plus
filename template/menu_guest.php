90e939cda4bab3657f68ee091e6b455a0d42c180
PloyRapeepat <117632209+PloyRapeepat@users.noreply.github.com>
2022-11-13 16:07:53 +0700

    เพิ่มเมนู

---
+++ template/menu_guest.php
@@ -0,0 +1,27 @@
+<nav class="navbar bg-light navbar-expand-lg">
+        <div class="container-fluid">
+            <a href="." class="navbar-brand">สังคมผู้สูงอายุ</a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#x">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="x" class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="?p=about" class="nav-link">เกี่ยวกับเรา</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=informetion" class="nav-link">ข่าวสาร</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=chat" class="nav-link">พูดคุย</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=login" class="nav-link">เข้าสู่ระบบ</a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+
+    </nav>
---
+++ template/menu_user.php
@@ -0,0 +1,33 @@
+<nav class="navbar bg-light navbar-expand-lg">
+        <div class="container-fluid">
+            <a href="." class="navbar-brand">สังคมผู้สูงอายุ</a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#x">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="x" class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="?p=about" class="nav-link">เกี่ยวกับเรา</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=informetion" class="nav-link">ข่าวสาร</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=chat" class="nav-link">พูดคุย</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=docter" class="nav-link">ปรึกษาหมอ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=edituser" class="nav-link">แก้ไขข้อมูลส่วนตัว</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?p=logout" class="nav-link">ออกจากระบบ</a>
+                    </li>
+                    
+                </ul>
+
+            </div>
+        </div>
+
+    </nav>
